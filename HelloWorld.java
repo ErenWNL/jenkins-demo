a5ecd851dbb52030cba5b50f1b1b47c88fb5fa5b
ErenWNL <94686773+ErenWNL@users.noreply.github.com>
2025-08-16 19:27:17 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World from Jenkins and Git!");
         System.out.println("Current time: " + new java.util.Date());
+        System.out.println("Goodbye World!");
     }
 }   
